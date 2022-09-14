b0ea3ee0b69c0c1427432a036d5461cad491d516
Daniel Figueroa <danfigueroa81@gmail.com>
2022-09-14 16:04:06 -0400

    format form component

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -3,15 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function FormComponent() {
     return (
       <form>
-      <div class="form-group">
+        {/* mx does margin for the x-values meaning left and right. Vice versa for my and y-values*/}
+      <div class="form-group mx-5 my-4">
         <label for="inputCity">City Name</label>
         <input type="city" class="form-control" id="inputCity" placeholder="Enter City"></input>
       </div>
-      <div class="form-group">
+      <div class="form-group mx-5 my-4">
         <label for="inputProvince">Province Name</label>
         <input type="province" class="form-control" id="inputProvince" placeholder="Enter Province"></input>
       </div>
+      {/* This utilizes flexbox to change the area of the screen it would occupy. Justify content */}
+      <div class="d-flex justify-content-center">
       <button type="submit" class="btn btn-primary">Enter</button>
+      </div>
       </form>
     );
 }
